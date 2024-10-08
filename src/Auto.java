90f64c904e68974cf9e3d78edb3444eaedd2ff35
Laboratorio <laboratorio@udla-ec.int>
2024-10-08 08:21:26 -0500

    First commit

---
+++ src/Auto.java
@@ -0,0 +1,20 @@
+public class Auto {
+    private String marca;
+    private String modelo;
+    private String color;
+    private String clindraje;
+
+    //constructores
+
+
+    //metds set y get
+
+
+
+    // Metdos propios u de calculo
+
+
+
+    // Modificacin de metods nativos
+
+}
